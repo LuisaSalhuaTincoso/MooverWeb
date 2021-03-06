5b8a72a636956080235c7e537615621638561372
Luma <lsalhuatincoso@gmail.com>
2021-03-05 20:09:27 -0500

    cambio de iframe por imagen

--- pages/estado_pedido.vue
+++ pages/estado_pedido.vue
@@ -4,14 +4,16 @@
         <div class= "estado">
             <h2>Pedido #340</h2>
             <h3 for="">En camino</h3>
-            <iframe src="https://goo.gl/maps/hJ2HGG9M8Wbv37QM6" frameborder="0"></iframe>
-            <a><nuxt-link to="/pedido_detalle">Mas detalle</nuxt-link></a>
+            <br>
+            <img src="https://ep01.epimg.net/tecnologia/imagenes/2019/11/13/actualidad/1573658897_566202_1573659544_noticia_normal_recorte1.jpg" width="400pt" height="400pt">
+            <br>
+            <nuxt-link class="link" to="/pedido_detalle">Detalle</nuxt-link>
         </div>
     </div>
 </template>
 <style>
 .estado{
-  width: 25%;
+  width: 35%;
   justify-content: center;
   align-items: center;
   text-align: center;
@@ -21,6 +23,10 @@
   border-radius: 5pt;
   border-style: solid;
   border-color: dimgray;
+  padding-bottom: 5%;
   
+}
+.link{
+    color:coral
 }
 </style>
